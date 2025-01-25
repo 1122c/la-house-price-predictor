ae009392f2c6151e1bd9867d065858efd32c2703
1122c <118850768+1122c@users.noreply.github.com>
2025-01-25 10:43:50 -0800

    changes to housepriceform js

--- frontend/src/components/HousePriceForm.js
+++ frontend/src/components/HousePriceForm.js
@@ -34,6 +34,66 @@ const HousePriceForm = () => {
     return (
         <div>
             <h2>House Price Prediction</h2>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    GrLivArea:
+                    <input
+                    type="number"
+                    name="GrLivArea"
+                    value={formData.GrLivArea}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    TotalBsmtSF:
+                    <input
+                    type="number"
+                    name="TotalBsmtSF"
+                    value={formData.TotalBsmtSF}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    OverallQual:
+                    <input
+                    type="number"
+                    name="OverallQual"
+                    value={formData.OverallQual}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <label>
+                    YearBuilt:
+                    <input>
+                    type="number"
+                    name="YearBuilt"
+                    value={formData.YearBuilt}
+                    onChange{handleChange}
+                    </input>
+                </label>
+                <br />
+                <label>
+                    LotArea:
+                    <input
+                    type="number"
+                    name="LotArea"
+                    value={formData.LotArea}
+                    onChange={handleChange}
+                    />
+                </label>
+                <br />
+                <button type="submit">Predict Price</button>
+            </form>
+            {prediction !== null &&(
+                <div>
+                    <h3>Predicted Price: ${prediction}</h3>
+                    </div>
+            )}
         </div>
-    )
-}
+    );
+};
+
+export default HousePriceForm;
